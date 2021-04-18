3d6668d545d8ba26cc134b59906db19c2431de26
Aykut <aykutkara2240@gmail.com>
2021-04-18 22:10:28 +0300

    ikinci guncelleme

--- GPProje/Form1.Designer.cs
+++ GPProje/Form1.Designer.cs
@@ -30,22 +30,32 @@ namespace GPProje
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(342, 274);
+            this.button1.Location = new System.Drawing.Point(321, 274);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(131, 31);
             this.button1.TabIndex = 0;
             this.button1.Text = "BUTON";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(332, 116);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +66,7 @@ namespace GPProje
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
