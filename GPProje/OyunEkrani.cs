83d5a6e8d699cbcaf69ac96bdff5898f1c26b34b
Aykut <aykutkara2240@gmail.com>
2021-05-19 20:50:39 +0300

    proje bitti.

--- GPProje/OyunEkrani.cs
+++ GPProje/OyunEkrani.cs
@@ -22,7 +22,10 @@ namespace GPProje
         public static char[] kelimeuzunlugu;//bu char dizisini,secilen kelimenin uzunluğu kadar label da "_" koymak için oluşturduk.
         public int sayac ;//bu değişkeni, "kelimeuzunluğu" değişkeninin uzunluğuna eşitleyip kullanıcı her bildiğinde azaltıp oyunu kazanıp kazanmadığını kontrol etmek için oluşturduk.
         int pilcubugu = 6;//bu değişkeni, kullanıcı her yanlış tahmin yaptığında pil görselindeki çubuğu azaltmak için oluşturduk.
-        
+
+        /// <summary>
+        /// Bu metodu, oyun başladığında veya tekrar oynanmak istediğinde kategori ve kelimenin belirlenmesi için oluşturduk.
+        /// </summary>
         public void KelimeVeKategori()
         {
             string[,] katagorivekelimeler = new string[3, 7] //bu dizide, katagori ve kelimeleri belirledik.(0-7 arası şehirler, 1-7 arası ülkeler, 2-7 arası araba markaları)
@@ -42,7 +45,6 @@ namespace GPProje
             if (kategori == 0)//random olarak seçilen sayıya göre katagoriyi belirlemek için oluşturduk(0 ise şehir , 1 ise ülke , 2 ise araba markası)
             {
                 secilenkelime = katagorivekelimeler[kategori, kelime];//"secilenkelime" değişkenini random olarak ayarladığımız "kategori" ve "kelime" değerlerininden yararlanarak "katagorivekelimeler" dizisindeki değere atıyoruz.
-                MessageBox.Show(secilenkelime);
                 lbl_kategori.Text = "Bu Bir Şehir";//bu if bloğuna girdiği için ""lbl_kategori" nin textini değiştiriyoruz.
                 kelimeuzunlugu = new char[katagorivekelimeler[kategori, kelime].Length];//"kelimeuzunlugu" dizisinin boyutunu seçilen kelimenin uzunluğuna eşitliyoruz.
                 sayac = kelimeuzunlugu.Length;
@@ -61,7 +63,6 @@ namespace GPProje
             else if (kategori == 1)//random olarak seçilen sayıya göre katagoriyi belirlemek için oluşturduk(0 ise şehir , 1 ise ülke , 2 ise araba markası)
             {
                 secilenkelime = katagorivekelimeler[kategori, kelime];//"secilenkelime" değişkenini random olarak ayarladığımız "kategori" ve "kelime" değerlerininden yararlanarak "katagorivekelimeler" dizisindeki değere atıyoruz.
-                MessageBox.Show(secilenkelime);
                 lbl_kategori.Text = "Bu Bir Ülke";//bu else if bloğuna girdiği için ""lbl_kategori" nin textini değiştiriyoruz.
                 kelimeuzunlugu = new char[katagorivekelimeler[kategori, kelime].Length];//"kelimeuzunlugu" dizisinin boyutunu seçilen kelimenin uzunluğuna eşitliyoruz.
                 sayac = kelimeuzunlugu.Length;
@@ -79,7 +80,6 @@ namespace GPProje
             else //random olarak seçilen sayıya göre katagoriyi belirlemek için oluşturduk(0 ise şehir , 1 ise ülke , 2 ise araba markası)
             {
                 secilenkelime = katagorivekelimeler[kategori, kelime];//"secilenkelime" değişkenini random olarak ayarladığımız "kategori" ve "kelime" değerlerininden yararlanarak "katagorivekelimeler" dizisindeki değere atıyoruz.
-                MessageBox.Show(secilenkelime);
                 lbl_kategori.Text = "Bu Bir Araba Markası";//bu else bloğuna girdiği için ""lbl_kategori" nin textini değiştiriyoruz.
                 kelimeuzunlugu = new char[katagorivekelimeler[kategori, kelime].Length];//"kelimeuzunlugu" dizisinin boyutunu seçilen kelimenin uzunluğuna eşitliyoruz.
                 sayac = kelimeuzunlugu.Length;
@@ -97,83 +97,72 @@ namespace GPProje
             str = secilenkelime;//secilenkelime yi "Islemler" metodunda kullanmak için str ye eşitledik.
         }
 
+        /// <summary>
+        /// Bu metodu, oyuncu buton a bastıktan sonra gerçekleşmesi gerekli işlemler ve kontrollerin yapılması için oluşturduk.
+        /// </summary>
         public void Islemler()
         {
             
-            string secilenKelime = str;
-            string girilentext = textBox1.Text.ToUpper();
-            char girilenharf = ' ';
-            if (girilentext == "")
+            string secilenKelime = str;//bu değişkeni, "KelimeVeKategori" metodunda belirlenen kelimeyi "Islemler" metodunda da kullanmak için oluşturduk.
+            string girilentext = textBox1.Text.ToUpper();//bu değişkeni, textbox a girilen harfi kullanmak ve kontrol etmek için oluşturduk.
+            char girilenharf = ' ';//bu değişkeni,girilen harfin seçilen kelimenin harflerinden biri mi diye kontrol etmek için oluşturduk.
+            if (girilentext == "")//textbox ın boş bırakılmaması için oluşturduk.
             {
-                MessageBox.Show("Boş bırakamazsınız");
+                MessageBox.Show("Boş bırakılamaz."+"\nLütfen bir harf giriniz.");
                 return;
                 
             }
-            else
+            else//textbox boş değilse gerçekleşmesi gerken işlemler için oluşturduk.
             {
-                girilenharf = Convert.ToChar(girilentext);
-            }
-            
-            lbl_girilenharfler.Text += girilentext;
-            
-            char[] girilenharfkontrol = new char[secilenKelime.Length];
-            //for (int i = 0; i < girilenharfkontrol.Length; i++)
-            //{
-            //    if (girilenharf == girilenharfkontrol[i])
-            //    {
-            //        MessageBox.Show("Bu harf girildi.");
-            //    }
-            //    else
-            //    {
-            //        break;
-            //    }
-            //}
-            //for (int i = 0; i < 1; i++)
-            //{
-            //    girilenharfkontrol[a] = girilenharf;
-            //    a++;
-            //}
-            bool var_mi = false;
-
-            for (int i = 0; i < secilenKelime.Length; i++) //*
-            {
-                if (girilenharf == secilenKelime[i])
+                foreach (var item in lbl_girilenharfler.Text)//bu döngü, girilen bütün harfleri char olarak döndürsün diye oluşturduk.
                 {
-                    var_mi = true;
-                    kelimeuzunlugu[i] = girilenharf;
-                    lbl_kelime.Text = lbl_kelime.Text.Remove(i*2,1);
-                    lbl_kelime.Text = lbl_kelime.Text.Insert(i * 2, girilenharf.ToString()).ToUpper();
-                    secilenKelime.Remove(i);
-                    sayac--;
-                    if (sayac == 0)
+                    if (item.ToString() == girilentext)//yeni girilen metin foreach döngüsü yardımıyla daha önce girilmiş mi kontrol etsin diye oluşturduk.
                     {
-                        lbl_kelime.Text = secilenKelime;
-                        MessageBox.Show(secilenKelime+ "\nOyunu Kazandınız.");
-                        DialogResult secenek = MessageBox.Show("Tekrar Oynamak İstiyor musunuz ?", "Tebrikler", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                        if (secenek == DialogResult.Yes)
+                        MessageBox.Show("Bu harfi daha önce girdiniz.");
+                        return;
+                    }
+                }
+                girilenharf = Convert.ToChar(girilentext);
+                lbl_girilenharfler.Text += girilentext;//oyuncu daha önce hangi harfleri girdiğini görsün diye oluşturduk.
+            }
+
+            bool var_mi = false;//pil görselini duruma göre değiştirmek için oluşturduk.
+
+            for (int i = 0; i < secilenKelime.Length; i++)//bu döngüyü, girilen harfin seçilen kelimenin harflerinden biriyle uyuşuyor mu diye oluşturduk.
+            {
+                if (girilenharf == secilenKelime[i])//girilen harf var mı diye kontrol ediyoruz.
+                {
+                    var_mi = true;//girilen harf olduğu için aşşağıdaki tekrardan if bloğuna girip resmi değiştirmemesi için true yaptık.
+                    kelimeuzunlugu[i] = girilenharf;//girilen harf kelimede bulunduğu için "kelimeuzunluğu" değişkeninin o an ki i. değerini "_" yerine girilen harfe eşitledik.
+                    lbl_kelime.Text = lbl_kelime.Text.Remove(i*2,1);//formdaki label dan "_" yi sildik.
+                    lbl_kelime.Text = lbl_kelime.Text.Insert(i * 2, girilenharf.ToString()).ToUpper();//formdaki label ın sildiğimiz yerine girilen harfi yazdırdık. 
+                    secilenKelime.Remove(i);
+                    sayac--; //kullanıcı her bildiğinde sayacı azaltıyoruz ki kaç tane bilmesi gereken harf kaldı belli olsun.
+                    if (sayac == 0)//sayac sıfırlanınca bilmediği hiç harf kalmadığı için oyunu kazanmış oluyor.
+                    {
+                        lbl_kelime.Text = secilenKelime;//bütün kelimeeleri bilince formda ki label da secilen kelimeyi yazdırıyoruz.
+                        MessageBox.Show("Cevap : "+secilenKelime+ "\nOYUNU KAZANDINIZ.","Tebrikler");
+                        DialogResult secenek = MessageBox.Show("Tekrar Oynamak İstiyor musunuz ?", "Tebrikler", MessageBoxButtons.YesNo, MessageBoxIcon.Question);//oyuncuya tekrar oynamak isteyip istemediğini soruyoruz.
+                        if (secenek == DialogResult.Yes)//evet i seçerse sonraki_bolum metodunu çağırarak oyunu sıfırlayıp yeni oyuna geçiyor.
                         {
                             sonraki_bolum();
                         }
-                        else if (secenek == DialogResult.No)
+                        else if (secenek == DialogResult.No)//hayır ı seçerse form u kapatıyoruz.
                         {
                             this.Close();
                         }
                     }
                 }
-
-                
             }
-            
-            if (!var_mi)
+            if (!var_mi) // eğer textbox a girdiği değer secilen kelimenin harflerinden biri değilse buraya giriyor ve görselden pil çubuğu eksiliyor.
             {
-                pilcubugu--;
-                switch (pilcubugu)
+                pilcubugu--;//bu if bloğuna her girdiğinde tekrardan yanlış tahmin ettiği için pilçubugu nu azaltıyoruz ki ona göre görsel koyalım.
+                switch (pilcubugu)//bu switch-case bloklarında pilcubugu değerlerine göre ekrana görselleri getiriyoruz.
                 {
-                    case 1:
+                    case 1: //bu durumda hiç hakkı kalmadığı için oyunu kaybediyor ve form u kapatıyoruz.
                         pcb_pil.ImageLocation = @"resimler\bos_pil.jpg";
                         MessageBox.Show("Cevap : " + secilenKelime + "\nOyunu Kaybettin!");
                         this.Close();
-                        
                         break;
                     case 2:
                         pcb_pil.ImageLocation = @"resimler\2_pil.jpg";
